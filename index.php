413ad4986f5f4800f81893f657bf2fbcb446b667
makolivares <39139801+makolivares@users.noreply.github.com>
2018-05-10 07:13:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width" />
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-grid.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/haven.css">
